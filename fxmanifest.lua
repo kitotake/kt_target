d9359586deb5c6fcde1a08b3af8a8d22415f28fa
kitotake <ahmed12199725@gmail.com>
2026-04-26 22:14:54 +0200

    manifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -23,6 +23,53 @@ shared_scripts {
 
 client_scripts {
     'client/main.lua',
+    -- Core
+    'client/core/loop.lua',
+    'client/core/raycast.lua',
+    'client/core/detection.lua',
+    'client/core/resolver.lua',
+    'client/core/executor.lua',
+
+    -- Registry (documentaire)
+    'client/registry/entities.lua',
+    'client/registry/globals.lua',
+    'client/registry/models.lua',
+    'client/registry/zones.lua',
+
+    -- State
+    'client/state/target.lua',
+
+    -- NUI
+    'client/nui/bridge.lua',
+    'client/nui/focus.lua',
+    'client/nui/messages.lua',
+
+    -- Utils
+    'client/utils/entity.lua',
+    'client/utils/math.lua',
+    'client/utils/table.lua',
+
+    -- API
+    'client/api/exports.lua',
+
+    -- Framework adapters
+    'client/framework/esx.lua',
+    'client/framework/nd.lua',
+    'client/framework/ox.lua',
+    'client/framework/qbx.lua',
+    'client/framework/union.lua',
+
+    -- Admin
+    'client/admin/object_target.lua',
+
+    -- Commands
+    'client/commands/target.lua',
+
+    -- Compat
+    'client/compat/qtarget.lua',
+
+    -- Debug
+    'client/debug/debug.lua',
 }
 
 server_scripts {
